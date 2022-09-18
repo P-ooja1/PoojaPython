795f658a197102a0f1b6879996bb6b5d374a636d
M.Poojadass <poojadass1999@gmail.com>
2022-09-18 11:48:36 +0530

    Calculator completed

--- CalculatorApp.py
+++ CalculatorApp.py
@@ -1,6 +1,50 @@
 # 1.get three string inputs
-# 2.first two inputs tobe converted to integer  and third input is string with symbols
-# 3.add , sub, mul, division and remaider division
+# 2.first two inputs to be converted to integer  and third input is string with symbols
+# 3.add , sub, mul, division and remainder division
 # 4.change the negative value as positive
-n = 10
-print("the added number is {0}".format(n))
+
+a = str(input("Enter the value"))
+b = str(input("Enter the value"))
+c = str(input("Enter the math function"))
+if c == '+':
+    res: int = int(a)+int(b)
+    print(res)
+    if res < 0:
+        d = res*(-1)
+        print(d)
+    else:
+        print("Null value")
+elif c == '-':
+    res = int(a)-int(b)
+    print(res)
+    if res < 0:
+        d = res * (-1)
+        print(d)
+    else:
+        print("Null value")
+elif c == '*':
+    res = int(a)*int(b)
+    print(res)
+    if res < 0:
+        d = res * (-1)
+        print(d)
+    else:
+        print("Null value")
+elif c == '/':
+    res = int(a)/int(b)
+    print(res)
+    if res < 0:
+        d = res * (-1)
+        print(d)
+    else:
+        print("Null value")
+elif c == '%':
+    res = int(a)%int(b)
+    print(res)
+    if res < 0:
+        d = res * (-1)
+        print(d)
+    else:
+        print("Null value")
+else:
+    print("Invalid value")
