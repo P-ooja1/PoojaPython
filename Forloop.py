1ab049d04edf15ce5ab3013cc0069f9ee0c07902
M.Poojadass <poojadass1999@gmail.com>
2022-09-21 20:49:47 +0530

    Forloop

---
+++ Forloop.py
@@ -0,0 +1,30 @@
+#1.Print elements one by one present in a list
+#2.Print a range with given a and b
+#3.Print 13 table with 10
+#4.append empty list with given range with getting input
+#5.break and continue(create a list with fruits, if "" == list in fruit break, continue
+
+#1
+#newList = ["app","Web","0",1,4]
+#for l in range(0, len(newList), 1):
+#print(newList[l])
+
+#3
+#a = int(input("Enter value"))
+#b = int(input("Enter range"))
+#for mul in range(1, b+1, 1):
+#print(mul,"*",a,"=",mul*a)
+
+#5
+#Break & Continue
+#a = ["dragonfrt","Kiwi","banana","apple","jackfrt"]
+#for x in a:
+#if x == "Kiwi" :
+#continue
+#print(x)
+---------------------------------------------------------------------
+#a = ["dragonfrt","Kiwi","banana","apple","jackfrt"]
+#for x in a:
+#if x == "Kiwi" :
+#break
+#print(x)
