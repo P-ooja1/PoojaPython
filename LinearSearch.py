8e78bfa2bdbe23e610d3fca4a04f73cdd2868c79
M.Poojadass <poojadass1999@gmail.com>
2022-09-22 06:21:35 +0530

    Linear search completed

---
+++ LinearSearch.py
@@ -0,0 +1,12 @@
+# Linear Search
+
+x = int(input("Enter the range "))
+y = []
+for j in range(0, x, 1):
+    b = input("Enter the list value")
+    y.append(b)
+print(y)
+z = input("Enter the search list value")
+for k in range(0, len(y),1) :
+    if y[k] == z :
+        print(y[k])
