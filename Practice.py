f450c8c423c36bb20dbf7d4ae157459ab8fed269
M.Poojadass <poojadass1999@gmail.com>
2022-09-29 23:53:42 +0530

    Practice sums

---
+++ Practice.py
@@ -0,0 +1,37 @@
+# # The provided code stub reads and integer, n , from STDIN. For all non-negative integers ,i<n  print n^2 .
+
+# n = int(input("Enter the value"))
+# i = 0
+# for i in range(0, n, 1) :
+#     if i < n :
+#         print(i*i)
+#     else :
+#         print("nf")
+
+-------------------------------------------------------------------------------------------
+# Given two integer numbers return their product only if the product is equal to or lower than 1000,
+# else return their sum.
+
+# n1 = int(input("Enter the number1"))
+# n2 = int(input("Enter the number2"))
+# a = n1*n2
+# b = n1+n2
+# if a < 1000 :
+#     print(a)
+# else :
+#     print(b)
+------------------------------------------------------------------------------------
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# x = int(input("Enter a range"))
+# y = []
+# for i in range(0,x,1) :
+#     z = int(input("Enter the value"))
+#     y.append(z)
+# print(y)
+# for j in y :
+#     if j % 5 == 0 :
+#         print(j)
+#     else :
+#         print("Not divisible")
+------------------------------------------------------------------------------------------
