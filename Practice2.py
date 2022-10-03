812fc200d929b160c3dc25ed21926b870ddb840f
M.Poojadass <poojadass1999@gmail.com>
2022-10-03 23:00:35 +0530

    Practice sums

---
+++ Practice2.py
@@ -0,0 +1,56 @@
+#Print characters from a string that are present at an even index number
+
+# x = input("Enter the string value")
+# for i in range(0, len(x), 2) :
+#     print(x[i])
+
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# n =input("Enter the value to remove")
+# print(n[2:])
+
+# doubt
+# Write a function to return True if the first and last number of a given list is same.
+# If numbers are different then return False.
+
+#IterIterate the given list of numbers and print only those numbers which are divisible by 5
+
+# a = [10,20,85,99,66,5,30,55]
+# for i in a :
+#     if  i % 5 == 0 :
+#         print(i)
+#     else :
+#         print("NF")
+
+# Return the count of a given substring from a string
+
+# x = input("Enter the value to find repeated words")
+# y = x.count('is')
+# print(y)
+
+# pattern
+
+# for i in range(8) :
+#     for j in range(i) :
+#         print(i,end=" ")
+#     print("\n")
+
+
+# Given a two list of numbers, write a program to create a new list
+# such that the new list should contain odd numbers from the first list and
+# even numbers from the second list.
+
+# a= []
+# b = [10,11,13,14,20]
+# c = [5,20,30,40,85]
+# for i in b :
+#     if i % 2 != 0 :
+#         a.append(i)
+# for i in c :
+#     if i % 2 == 0 :
+#         a.append(i)
+# print(a)
+
+# doubt  Write a Program to extract each digit from an integer in the reverse order.
+# For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
+
