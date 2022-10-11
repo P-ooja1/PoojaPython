dbcf5edb0bb63069b38cf0b39bbe9aeb0d7fddeb
M.Poojadass <poojadass1999@gmail.com>
2022-10-11 22:34:23 +0530

    Brk Continue

---
+++ BreakanContinue.py
@@ -0,0 +1,18 @@
+# j = 0
+# k = 0
+# num1 = [1,2,3,4,5,6,7,8,9,10]
+# for i in num1 :
+#     # print(i)
+#     # # break
+#     # continue
+#     # if 4 == i :
+#     #     # print(i)
+#     #     continue
+#     # print(i)
+#     j = j + 1
+#     # continue
+#     continue
+#     k = k + 1
+#     print(i)
+# print(j)
+# print(k)
