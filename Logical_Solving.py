a22fee46a25e5d26bd6ef7b026f681250e5ee107
M.Poojadass <poojadass1999@gmail.com>
2022-10-16 23:32:25 +0530

    Brk Continue

---
+++ Logical_Solving.py
@@ -0,0 +1,35 @@
+# palindrome for string ip
+
+# a =int(input("Enter the input"))
+# d = str(a)
+# b =[]
+# for i in range(len(d)-1, -1, -1) :
+#     b.append(d[i].upper())
+# c=[]
+# for i in range(0, len(d), 1) :
+#     c.append(d[i].lower())
+# if b == c :
+#     print("It is palindrome")
+# else :
+#     print("It is not palindrome")
+# print(d)
+# print(b)
+# print(c)
+
+#Simplified string ip palindrome
+
+# x = input("enter a str").upper()
+# y = x[::-1].upper()
+# if x == y :
+#     print("it is palindrome")
+# else :
+#     print("not palindrome")
+# print(x)
+# print(y)
+
+
+# Mathematical solution
+
+a = 1230
+print(a//10)
+print(a%10)
